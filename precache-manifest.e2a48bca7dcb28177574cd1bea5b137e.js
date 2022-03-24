75667659e785f55de599a61479382d09f824b1df
shari4ov <kenansharifov400@gmail.com>
2022-03-24 11:04:05 +0400

    Updates

---
+++ precache-manifest.e2a48bca7dcb28177574cd1bea5b137e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "710de0351ba6fcd20c77",
+    "url": "/memeGenerator/static/css/main.20e4f730.chunk.css"
+  },
+  {
+    "revision": "710de0351ba6fcd20c77",
+    "url": "/memeGenerator/static/js/main.710de035.chunk.js"
+  },
+  {
+    "revision": "acf57d5ced29217995a4",
+    "url": "/memeGenerator/static/js/1.acf57d5c.chunk.js"
+  },
+  {
+    "revision": "e16ec418ad2b7bb44ecd",
+    "url": "/memeGenerator/static/js/runtime~main.e16ec418.js"
+  },
+  {
+    "revision": "c0e1d1d351a9ef07131af1e6b2fe0589",
+    "url": "/memeGenerator/index.html"
+  }
+];
