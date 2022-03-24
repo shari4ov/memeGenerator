cd470db8b658c7a495e53ad491ef147bf2371034
shari4ov <kenansharifov400@gmail.com>
2022-03-24 11:48:01 +0400

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import troll from "../images/troll-face.png";
 export default function Header(){
        return (
-              <nav className="nav align-items-center p-3 text-light" id="nav">
+              <nav className="nav align-items-center justify-content-center p-3 text-light" id="nav">
                             <img src={troll} className="troll" />
                             <h1 className="nav--title">Meme Generator </h1>
               </nav>
